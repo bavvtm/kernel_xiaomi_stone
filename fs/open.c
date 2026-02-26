b892a8b14f7ad57aeaae58cbb61c845fca8fe583
bavvtm <bavvtm@outlook.com>
2026-02-27 06:08:36 +0700

    {input,fs,reboot,sys}: Rework KSU manual hook implementation
    Changes:
    - Rename CONFIG_KSU to CONFIG_KSU_MANUAL_HOOK
    - Move ksu_handle_execveat hook to compat_do_execve (fs/exec)
    - Rename ksu_vfs_read_hook to ksu_init_rc_hook (fs/read_write)
    - Add ksu_handle_newfstat_ret hook (fs/stat)
    - Add ksu_handle_fstat64_ret hook (fs/stat)
    - Add ksu_handle_setresuid hook (kernel/sys)

--- fs/open.c
+++ fs/open.c
@@ -440,7 +440,7 @@ out:
 	return res;
 }
 
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 __attribute__((hot)) 
 extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
 				int *mode, int *flags);
@@ -448,7 +448,7 @@ extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
 
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 #endif
 	return do_faccessat(dfd, filename, mode);
